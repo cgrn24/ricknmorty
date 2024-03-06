bc05bf59a822d24e988d8ca0b56819c769bf9950
cgrn24 <stlhnt42@gmail.com>
2024-03-07 04:49:06 +0700

    refactor: tests update

--- src/test/char-location.test.tsx
+++ src/test/char-location.test.tsx
@@ -1,18 +1,9 @@
 import { waitFor, renderHook, screen, render } from '@testing-library/react'
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@testing-library/jest-dom'
 import nock from 'nock'
 import { CharacterModal } from '../components/modals/character/CharacterModal'
-import { getLocation } from 'rickmortyapi'
-
-const useGetLocationTest = () => {
-  const id = 2
-  const getLoc = async () => {
-    const data = await getLocation(id)
-    return data
-  }
-  return useQuery({ queryKey: ['location', id], queryFn: getLoc })
-}
+import { useLocationsQuery } from '../services/useLocationsQuery'
 
 test('getting location by id', async () => {
   const queryClient = new QueryClient({
@@ -27,21 +18,34 @@ test('getting location by id', async () => {
   //@ts-ignore
   const wrapper = ({ children }) => <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   nock('https://rickandmortyapi.com')
-    .get('/api/character/10')
+    .get('/api/location/?name=Abadango')
     .reply(200, {
-      id: 2,
-      name: 'Abadango',
-      type: 'Cluster',
-      dimension: 'unknown',
-      residents: ['https://rickandmortyapi.com/api/character/6'],
-      url: 'https://rickandmortyapi.com/api/location/2',
-      created: '2017-11-10T13:06:38.182Z',
+      info: {
+        count: 1,
+        pages: 1,
+        next: null,
+        prev: null,
+      },
+      results: [
+        {
+          id: 2,
+          name: 'Abadango',
+          type: 'Cluster',
+          dimension: 'unknown',
+          residents: ['https://rickandmortyapi.com/api/character/6'],
+          url: 'https://rickandmortyapi.com/api/location/2',
+          created: '2017-11-10T13:06:38.182Z',
+        },
+      ],
     })
-  const { result } = renderHook(() => useGetLocationTest(), { wrapper })
+  const name = 'Abadango'
+  const { result } = renderHook(() => useLocationsQuery({ name }), { wrapper })
   await waitFor(() => {
     return result.current.isSuccess
   })
-  await waitFor(() => expect(result.current.data?.data.name).toBe('Abadango'))
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+  await waitFor(() => expect(result.current.data?.data.results[0].type).toBe('Cluster'))
 })
 test('renders character details', async () => {
   const id = 10
